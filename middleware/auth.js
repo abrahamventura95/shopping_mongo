5461c42fbee379c91c822470162c195170cc30d2
Abraham Ventura <amsventura.95@gmail.com>
2020-09-08 13:41:33 -0400

    Users
    User CRUD

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+exports.ensureAuthenticated = function(req,res,next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) =>{
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+exports.isShop = function(req, res, next){
+	if(req.user.type != 'shop') return res.sendStatus(403);
+    next();			    
+}
---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    type: ['user','shop','admin'],
+    rif: {type: String, default: false},
+    custom: String,
+    tlfn: Number,
+    name: String,
+    created_at: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('User', UserSchema, "users");
